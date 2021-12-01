da6a36f8b391420a116e1140dd596e14850aa97c
Marcus Ho <marcus.ho@oocl.com>
2021-12-01 16:24:10 +0800

    [OopPractice][Marcus]feat: AC2.

--- src/main/java/com/afs/oopractice/HelloWorld.java
+++ src/main/java/com/afs/oopractice/HelloWorld.java
@@ -1,10 +1,13 @@
 package com.afs.oopractice;
 
 import com.afs.oopractice.Objects.Car;
+import com.afs.oopractice.Objects.Truck;
 
 public class HelloWorld {
     public static void main(String[] args) {
         Car car = new Car("Car", 30);
         car.speedUp();
+        Truck truck = new Truck("Truck", 10);
+        truck.speedUp();
     }
 }
